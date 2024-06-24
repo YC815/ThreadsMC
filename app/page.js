5947e5813d2dbbf68606683403740fde7a435fcc
YC815 <minnecraft.YC815@gmail.com>
2024-06-24 19:52:15 +0800

    首頁完成合照以及基本短文簡介

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { useMediaQuery } from "@react-hook/media-query";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -13,13 +14,27 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+  const isPadOrComputer = useMediaQuery("(min-width: 768px)");
+  const maxWidthPercentage = isPadOrComputer ? "95%" : "100%";
+
+  React.useEffect(() => {
+    // 在组件挂载时检查当前主题，并根据主题设置文字颜色
+    if (theme === "light") {
+      document.documentElement.classList.add("light-theme");
+      document.documentElement.classList.remove("dark-theme");
+    } else if (theme === "dark") {
+      document.documentElement.classList.add("dark-theme");
+      document.documentElement.classList.remove("light-theme");
+    }
+  }, [theme]);
+
   return (
     <div className="min-h-screen bg-[hsl(var(--background))]">
       <nav className="bg-[hsl(var(--nav-color))] p-4 flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Avatar className="order-1">
-            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarImage src="https://github.com/YC815/ThreadsMC/blob/main/public/user_icon/small_user_icon.jpg?raw=true" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div className="text-black dark:text-white text-xl order-2">
@@ -58,7 +73,22 @@ export default function Home() {
           </DropdownMenu>
         </div>
       </nav>
-      <div className="p-3">{/* 其他內容 */}</div>
+      <div className="p-3 flex justify-center">
+        <div className={`max-w-[${maxWidthPercentage}]`}>
+          <Image
+            src="/group_photo_2024_6.png"
+            layout="responsive"
+            width={1920}
+            height={1080}
+          />
+        </div>
+      </div>
+      <h1 className="text-center text-4xl font-bold text-light dark:text-dark">
+        ThreadsMC伺服器
+      </h1>
+      <h2 className="text-center text-2xl font-bold text-light dark:text-dark">
+        橫跨Java版以及基岩版的新世代Minecraft伺服器
+      </h2>
     </div>
   );
 }
