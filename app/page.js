e5df1d51e70c61f916f2e39b6b65dbd5187f2ebc
YC815 <minnecraft.YC815@gmail.com>
2024-06-24 20:15:01 +0800

    主頁完成 + 開始製作資訊

--- app/page.js
+++ app/page.js
@@ -14,27 +14,17 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
+  const { setTheme } = useTheme();
   const isPadOrComputer = useMediaQuery("(min-width: 768px)");
   const maxWidthPercentage = isPadOrComputer ? "95%" : "100%";
 
-  React.useEffect(() => {
-    // 在组件挂载时检查当前主题，并根据主题设置文字颜色
-    if (theme === "light") {
-      document.documentElement.classList.add("light-theme");
-      document.documentElement.classList.remove("dark-theme");
-    } else if (theme === "dark") {
-      document.documentElement.classList.add("dark-theme");
-      document.documentElement.classList.remove("light-theme");
-    }
-  }, [theme]);
-
   return (
     <div className="min-h-screen bg-[hsl(var(--background))]">
-      <nav className="bg-[hsl(var(--nav-color))] p-4 flex justify-between items-center">
+      {/* Navigation */}
+      <nav className="fixed-nav bg-[hsl(var(--nav-color))] p-4 flex justify-between items-center">
         <div className="flex items-center space-x-4">
           <Avatar className="order-1">
-            <AvatarImage src="https://github.com/YC815/ThreadsMC/blob/main/public/user_icon/small_user_icon.jpg?raw=true" />
+            <AvatarImage src="/user_icon/small_user_icon.jpg" />
             <AvatarFallback>CN</AvatarFallback>
           </Avatar>
           <div className="text-black dark:text-white text-xl order-2">
@@ -46,7 +36,7 @@ export default function Home() {
             首頁
           </a>
           <a href="/about" className="text-black dark:text-white">
-            關於我們
+            伺服器資訊
           </a>
           <a href="/contact" className="text-black dark:text-white">
             聯絡我們
@@ -73,22 +63,58 @@ export default function Home() {
           </DropdownMenu>
         </div>
       </nav>
-      <div className="p-3 flex justify-center">
-        <div className={`max-w-[${maxWidthPercentage}]`}>
+
+      {/* Main content */}
+      <div className="p-3">
+        {/* Image */}
+        <div className="relative w-full h-96 mb-4">
           <Image
             src="/group_photo_2024_6.png"
-            layout="responsive"
-            width={1920}
-            height={1080}
+            layout="fill"
+            objectFit="cover"
+            alt="Group Photo"
           />
         </div>
+        <h1 className="text-5xl font-bold text-black dark:text-white mb-4 text-center">
+          ThreadsMC伺服器
+        </h1>
+        <h2 className="text-3xl font-bold text-black dark:text-white mb-4 text-center">
+          橫跨Java版與基岩版的新世代Minecraft伺服器
+        </h2>
+        {/* Text sections */}
+        <div className="flex flex-wrap justify-center gap-4">
+          <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+            <p className="text-lg font-bold text-black dark:text-white mb-2">
+              保留原版架構
+            </p>
+            <p className="text-gray-700 dark:text-gray-300">
+              堅持不安裝領地、Home插件等花俏插件。伺服器只有安裝Tpa及一些效能插件，以最大限度地保留原版滋味。
+            </p>
+          </div>
+          <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+            <p className="text-lg font-bold text-black dark:text-white mb-2">
+              友善社區
+            </p>
+            <p className="text-gray-700 dark:text-gray-300">
+              玩家社群數量約30人左右，小型社群帶來更容易結交夥伴的環境，一起快樂生存！
+            </p>
+          </div>
+          <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
+            <p className="text-lg font-bold text-black dark:text-white mb-2">
+              經常舉辦活動
+            </p>
+            <p className="text-gray-700 dark:text-gray-300">
+              我們有時候會在週末舉辦地圖同樂會、吃雞比賽等各種活動，如果生存玩膩了也可以一起玩地圖！
+            </p>
+          </div>
+        </div>
+        <h1 className="text-5xl font-bold text-black dark:text-white mb-4 text-center mt-10">
+          現在就加入伺服器！
+        </h1>
+        <div className="flex justify-center">
+          <Button className="h-20 w-56 text-2xl ">查看資訊</Button>
+        </div>
       </div>
-      <h1 className="text-center text-4xl font-bold text-light dark:text-dark">
-        ThreadsMC伺服器
-      </h1>
-      <h2 className="text-center text-2xl font-bold text-light dark:text-dark">
-        橫跨Java版以及基岩版的新世代Minecraft伺服器
-      </h2>
     </div>
   );
 }
