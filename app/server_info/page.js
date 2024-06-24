3ead4e117b9f203987d0d14f58bd06ad1fef3f6e
YC815 <minnecraft.YC815@gmail.com>
2024-06-24 20:44:18 +0800

    v1

--- app/server_info/page.js
+++ app/server_info/page.js
@@ -13,10 +13,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
   const { setTheme } = useTheme();
   const isPadOrComputer = useMediaQuery("(min-width: 768px)");
   const maxWidthPercentage = isPadOrComputer ? "95%" : "100%";
@@ -71,54 +69,25 @@ export default function Home() {
       </nav>
 
       {/* Main content */}
-      <div className="p-3">
-        {/* Image */}
-        <div className="relative w-full h-96 mb-4">
-          <Image
-            src="/group_photo_2024_6.png"
-            layout="fill"
-            objectFit="cover"
-            alt="Group Photo"
-          />
-        </div>
-        <h1 className="text-5xl font-bold text-black dark:text-white mb-4 text-center">
-          ThreadsMC伺服器
-        </h1>
-        <h2 className="text-3xl font-bold text-black dark:text-white mb-4 text-center">
-          橫跨Java版與基岩版的新世代Minecraft伺服器
-        </h2>
-        {/* Text sections */}
+      <div className="p-3 pt-32">
         <div className="flex flex-wrap justify-center gap-4">
           <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
             <p className="text-lg font-bold text-black dark:text-white mb-2">
-              保留原版架構
+              Java IP
             </p>
             <p className="text-gray-700 dark:text-gray-300">
-              堅持不安裝領地、Home插件等花俏插件。伺服器只有安裝Tpa及一些效能插件，以最大限度地保留原版滋味。
+              tw-5.cs-nodes.net:30623
             </p>
           </div>
           <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
             <p className="text-lg font-bold text-black dark:text-white mb-2">
-              友善社區
+              Bedrock IP
             </p>
             <p className="text-gray-700 dark:text-gray-300">
-              玩家社群數量約30人左右，小型社群帶來更容易結交夥伴的環境，一起快樂生存！
+              IP: tw-1.cs-nodes.net <br />
+              Port: 30623
             </p>
           </div>
-          <div className="w-full md:w-1/3 bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md">
-            <p className="text-lg font-bold text-black dark:text-white mb-2">
-              經常舉辦活動
-            </p>
-            <p className="text-gray-700 dark:text-gray-300">
-              我們有時候會在週末舉辦地圖同樂會、吃雞比賽等各種活動，如果生存玩膩了也可以一起玩地圖！
-            </p>
-          </div>
-        </div>
-        <h1 className="text-5xl font-bold text-black dark:text-white mb-4 text-center mt-10">
-          現在就加入伺服器！
-        </h1>
-        <div className="flex justify-center">
-          <Button className="h-20 w-56 text-2xl ">查看資訊</Button>
         </div>
       </div>
     </div>
