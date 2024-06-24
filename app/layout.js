0317f282bf439e0f7a921a43593e3cdbc98c80af
YC815 <minnecraft.YC815@gmail.com>
2024-06-24 19:13:05 +0800

    nav + dark mode

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "/app/globals.css";
 import { Inter as FontSans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import { cn } from "@/lib/utils";
 
@@ -18,7 +19,14 @@ export default function RootLayout({ children }) {
           fontSans.variable
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
